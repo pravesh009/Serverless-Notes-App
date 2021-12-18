25cc2cbe6ac45bdb652ebbe3a7906661c6535c08
pravesh009 <pravesh.jain968@gmail.com>
2021-12-17 17:10:29 -0800

    Securing Api

---
+++ src/list.js
@@ -0,0 +1,38 @@
+import handler from "./util/handler";
+import dynamoDb from "./util/dynamodb";
+
+export const main = handler(async (event) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userId = :userId': only return items with matching 'userId'
+    //   partition key
+    KeyConditionExpression: "userId = :userId",
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userId': defines 'userId' to be the id of the author
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.authorizer.iam.cognitoIdentity.identityId,
+    },
+  };
+
+  const result = await dynamoDb.query(params);
+
+  // Return the matching list of items in response body
+  return result.Items;
+});
+
+//us-east-1
+//7i9mb0uqg898fb4doot3na98kq
+
+// npx aws-api-gateway-cli-test \
+// --username='admin@example.com' \
+// --password='Passw0rd!' \
+// --user-pool-id='us-east-1_bBEm2C1lP' \
+// --app-client-id='7i9mb0uqg898fb4doot3na98kq' \
+// --cognito-region='us-east-1' \
+// --identity-pool-id='us-east-1:86d6ffa9-d404-457e-904b-5061c627dd79' \
+// --invoke-url='https://61y2n2lf1f.execute-api.us-east-1.amazonaws.com' \
+// --api-gateway-region='us-east-1' \
+// --path-template='/notes' \
+// --method='POST' \
+// --body='{"content":"hello world","attachment":"hello.jpg"}'
---
+++ stacks/ApiStack.js
@@ -0,0 +1,38 @@
+import * as sst from "@serverless-stack/resources";
+
+//noteid : 2a58b0e0-5ee5-11ec-9f25-7157f508316a
+export default class ApiStack extends sst.Stack {
+  // Public reference to the API
+  api;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const { table } = props;
+
+    // Create the API
+    this.api = new sst.Api(this, "Api", {
+      defaultAuthorizationType: "AWS_IAM",
+      defaultFunctionProps: {
+        environment: {
+          TABLE_NAME: table.tableName,
+        },
+      },
+      routes: {
+        "POST   /notes": "src/create.main",
+        "GET    /notes/{id}": "src/get.main",
+        "GET    /notes": "src/list.main",
+        "PUT    /notes/{id}": "src/update.main",
+        "DELETE /notes/{id}": "src/delete.main",
+      },
+    });
+
+    // Allow the API to access the table
+    this.api.attachPermissions([table]);
+
+    // Show the API endpoint in the output
+    this.addOutputs({
+      ApiEndpoint: this.api.url,
+    });
+  }
+}
