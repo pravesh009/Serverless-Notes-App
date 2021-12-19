1214760acd111c1a68495c42cdd893d4583cb3e4
pravesh009 <pravesh.jain968@gmail.com>
2021-12-19 15:12:49 -0800

    Adding Unit Test

--- test/MyStack.test.js
+++ test/MyStack.test.js
@@ -1,11 +1,15 @@
 import { expect, haveResource } from "@aws-cdk/assert";
 import * as sst from "@serverless-stack/resources";
-import MyStack from "../stacks/MyStack";
+import StorageStack from "../stacks/StorageStack";
 
-test("Test Stack", () => {
+test("Test StorageStack", () => {
   const app = new sst.App();
   // WHEN
-  const stack = new MyStack(app, "test-stack");
+  const stack = new StorageStack(app, "test-stack");
   // THEN
-  expect(stack).to(haveResource("AWS::Lambda::Function"));
+  expect(stack).to(
+    haveResource("AWS::DynamoDB::Table", {
+      BillingMode: "PAY_PER_REQUEST",
+    })
+  );
 });
